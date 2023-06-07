bb87b96d4f1a0e5dd49bf2ddb01e1bff9856e29d
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-06-06 23:33:30 -0300

    add test transaction list

---
+++ test/transactions.spec.ts
@@ -0,0 +1,59 @@
+import { afterAll, beforeAll, it, describe, expect } from 'vitest'
+import request from 'supertest'
+import { app } from '../src/app'
+
+// Enunciado
+// Operação
+// Validação - o que eu espero - expect
+
+describe('Transactions routes', () =>{
+    //Executado antes de todos os testes
+    beforeAll(async () => {
+        //Só vou começar os testes quando estiver toda carregada a aplicação
+        await app.ready()
+    })
+
+    //Depois que todos os testes estiverem executados, vou descartar os testes
+    afterAll(async () => {
+        await app.close()
+    })
+
+    //Deve ser possível criar uma nova transação
+    it('should be able to create a new transaction', async () => {
+        await request(app.server)
+        .post('/transactions')
+        .send({
+            title: 'New transaction',
+            amount: 5000,
+            type: 'credit',
+        })
+        .expect(201)
+    })
+
+    it('should be able to list all transactions', async () => {
+        const createTransactionResponse = await request(app.server)
+          .post('/transactions')
+          .send({
+            title: 'New transaction',
+            amount: 5000,
+            type: 'credit',
+          })
+    
+        const cookies = createTransactionResponse.get('Set-Cookie')
+    
+        const listTransactionsResponse = await request(app.server)
+          .get('/transactions')
+          .set('Cookie', cookies)
+          .expect(200)
+    
+        //Espero que o corpo da minha lista e espero que tenha um objeto contendo
+        //o título e o amount``
+        expect(listTransactionsResponse.body.transactions).toEqual([
+        expect.objectContaining({
+            title: 'New transaction',
+            amount: 5000,
+          }),
+        ])
+      })
+
+})
